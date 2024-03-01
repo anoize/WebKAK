3c03321fab534de0e36142604d659010042c8ed0
anoize <152575195+anoize@users.noreply.github.com>
2024-03-01 12:24:53 +0700

    9
    9

--- index.php
+++ index.php
@@ -74,6 +74,8 @@ session_start();
 
   ?>
   <div>
+
+
     <a href="newpost.php" class="btn btn-success btn-sm" > <i class="bi bi-plus"></i>สร้างกระทู้ใหม่</a>
   </div>
   <?php  }   ?>
@@ -83,19 +85,10 @@ session_start();
         $conn=new PDO("mysql:host=localhost;dbname=webbord;charset=utf8","root","");
         $sql="SELECT t3.name,t1.title,t1.id,t2.login,t1.post_date FROM post as t1 INNER JOIN user as t2 ON (t1.user_id=t2.id) INNER JOIN category as t3 ON (t1.cat_id=t3.id) ORDER BY t1.post_date DESC";
         $result=$conn->query($sql);
-        
-        for ($i=1; $i <=10 ; $i++) { 
-            echo"<tr> <td class='d-flex justify-content-between'><a href=post.php?id=$i style=text-decoration:none>กระทู้ที่ $i </a>";
-            if ((isset($_SESSION['id'])) && ($_SESSION['role']=='a')) {
-                echo"&nbsp;&nbsp;<a href=delete.php?id=$i class='btn btn-danger btn-sm'><i class='bi bi-trash3'></i></a>";
-            }
-
-            else {
-                echo"";
-            }
-        
-            echo"</td></tr>";
-        } 
+        while ($row = $result->fetch()) {
+          echo"<tr><td>[ $row[0] ] <a href=post.php?id=$row[2] style=text-decoration:none>$row[1]</a><br>$row[3] - $row[4] </td></tr>";
+        }
+        $conn=null;
         ?>
 </table>
     </div>
